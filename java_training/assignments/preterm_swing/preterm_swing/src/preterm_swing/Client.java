048f9eaa1127da1822cac6176c5d02d993f72a67
djl0127 <bd.loughran@yahoo.com>
2019-07-31 08:11:04 -0500

    PreTerm pass 1 & 2

--- java_training/assignments/preterm_swing/preterm_swing/src/preterm_swing/Client.java
+++
@@ -1,9 +0,0 @@
-package preterm_swing;
-
-public class Client {
-	String firstName = "";
-	String lastName = "";
-	String clientID = "";
-	String clientInitials = "";
-	String seat = "";
-}
--- java_training/assignments/preterm_swing/preterm_swing/src/preterm_swing/Seat.java
+++
@@ -1,9 +0,0 @@
-package preterm_swing;
-
-public class Seat {
-	String clientID = "";
-	String clientInitials = "";
-	String label = "Vacant";
-	String firstName = "";
-	String lastName = "";
-}
